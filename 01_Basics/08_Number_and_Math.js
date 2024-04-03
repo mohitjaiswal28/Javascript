be892ccc2077585654bc0080e8634b6da1f66bbf
Mohit Jaiswal <mohitjaiswal2883@gmail.com>
2024-04-03 17:46:13 +0530

    Completed Number and Math

---
+++ 01_Basics/08_Number_and_Math.js
@@ -0,0 +1,12 @@
+
+// Random number between 0 and 1
+console.log(Math.random());
+
+// Random number between 1 and 10
+console.log(Math.floor(Math.random()*10) + 1);
+
+// Random number between min and max
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min)
