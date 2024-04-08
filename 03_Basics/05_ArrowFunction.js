1c7f97dc46daf85db2e6d36696639954d08a4190
Mohit Jaiswal <mohitjaiswal2883@gmail.com>
2024-04-08 12:44:50 +0530

    Arrow Function

---
+++ 03_Basics/05_ArrowFunction.js
@@ -0,0 +1,30 @@
+
+let user = {
+    username: "mohit",
+    age: 20,
+    welcomeMsg: function() {
+        console.log(this.username)
+        console.log(this)
+    }
+}
+
+user.welcomeMsg()
+console.log()
+user.username = "mj"
+user.welcomeMsg()
+
+console.log()
+console.log(this)       
+// In node environment, this = {} (empty object)
+// But, in web browser, this = Window
+
+
+// Arrow function
+let add = (num1, num2) => {
+    return num1 + num2
+}
+console.log(`Addition = ${add(3, 2)}`)
+
+// If we use () instead of {}, then no need to give the return keyword => Implicit return
+let mul = (num1, num2) => (num1 * num2)
+console.log(`Multiplication = ${mul(3, 2)}`)
