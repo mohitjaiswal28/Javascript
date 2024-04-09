c5b54c25d70d79d187aa22eb5d769882661f6c4f
Mohit Jaiswal <mohitjaiswal2883@gmail.com>
2024-04-09 21:21:10 +0530

    Arrow Function

--- 03_Basics/05_ArrowFunction.js
+++ 03_Basics/05_ArrowFunction.js
@@ -25,6 +25,6 @@ let add = (num1, num2) => {
 }
 console.log(`Addition = ${add(3, 2)}`)
 
-// If we use () instead of {}, then no need to give the return keyword => Implicit return
+// If we use () instead of {}, then no need to give the 'return' keyword => Implicit return
 let mul = (num1, num2) => (num1 * num2)
 console.log(`Multiplication = ${mul(3, 2)}`)
