a48b1743c1e3707390cdabebe5bb38491f8af9b9
Mohit Jaiswal <mohitjaiswal2883@gmail.com>
2024-04-09 22:15:18 +0530

    Control flow

---
+++ 03_Basics/07_Control_Flow.js
@@ -0,0 +1,26 @@
+
+// If else if else
+// Switch case
+// Ternary operator:-
+// let result = condn ? true : flase 
+
+// Falsy values:-
+/*
+    false
+    0
+    -0
+    BigInt 0n
+    null
+    undefined
+    NaN 
+*/
+
+// Truthy values:-
+/*
+    "0"
+    'false'
+    " "
+    []
+    {}
+    function() {}   // Empty function
+*/
